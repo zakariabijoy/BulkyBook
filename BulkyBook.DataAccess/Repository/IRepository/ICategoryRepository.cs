58b1832bec0c6417ec8f509c45c204efbf62a260
zakariabijoy <zakaria.bijoy@live.com>
2020-05-08 03:05:55 +0600

    Added Category Repository and Interface

---
+++ BulkyBook.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BulkyBook.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBook.DataAccess.Repository.IRepository
+{
+    public interface ICategoryRepository:IRepository<Category>
+    {
+        void Update(Category category);
+
+    }
+}
