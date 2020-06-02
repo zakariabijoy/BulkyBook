0e1b6fcc192fe8e45159eccd5de458bccb6a9ffe
zakariabijoy <zakaria.bijoy@live.com>
2020-06-02 22:22:29 +0600

    Section: Registration ; -- Implemented Admin Registration Functionality

--- BulkyBook/Startup.cs
+++ BulkyBook/Startup.cs
@@ -32,8 +32,9 @@ namespace BulkyBook
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddIdentity<IdentityUser,IdentityRole>().AddDefaultTokenProviders()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
+
             services.AddScoped<IUnitOfWork, UnitOfWork>();
             services.AddControllersWithViews().AddRazorRuntimeCompilation();
             services.AddRazorPages();
