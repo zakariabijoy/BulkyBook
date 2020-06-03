67147c880327c5711233fd631636da77967e2425
zakariabijoy <zakaria.bijoy@live.com>
2020-06-04 01:23:49 +0600

    Section: User Management; -- Implemented UserController  to show all users with there company and user role  (in application)

---
+++ BulkyBook/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BulkyBook.DataAccess.Data;
+using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBook.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UserController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public UserController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        
+
+        #region Api Calls
+
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var userList = _db.ApplicationUsers.Include(u => u.Company).ToList();
+            var userRoleList = _db.UserRoles.ToList();
+            var roles = _db.Roles.ToList();
+
+            foreach (var user in userList)
+            {
+                var roleId = userRoleList.FirstOrDefault(u => u.UserId == user.Id).RoleId;
+                user.Role = roles.FirstOrDefault(r => r.Id == roleId).Name;
+
+                if (user.Company == null)
+                {
+                    user.Company = new Company()
+                    {
+                        Name = ""
+                    };
+                }
+            }
+
+
+            return Json(new {data= userList });
+        }
+
+        
+
+        #endregion
+    }
+}
