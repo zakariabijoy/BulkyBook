2c413b6f3e9bee3fd50fe021d92f496049760405
zakariabijoy <zakaria.bijoy@live.com>
2020-06-04 02:14:49 +0600

    Section: User Management; -- user lock unlock js is added to user.js

--- BulkyBook/wwwroot/js/user.js
+++ BulkyBook/wwwroot/js/user.js
@@ -11,51 +11,43 @@ function loadDataTable() {
             "url": "/Admin/User/GetAll"
         },
         "columns": [
-            { "data": "name", "width": "60%" },
-            { "data": "email", "width": "60%" },
-            { "data": "phoneNumber", "width": "60%" },
-            { "data": "company.name", "width": "60%" },
-            { "data": "role", "width": "60%" }
-//            {
-//                "data": "id",
-//                "render": function (data) {
-//                    return `
-//                            <div class="text-center">
-//                                <a href="/Admin/Category/Upsert/${data}" class="btn btn-success text-white" style="cursor:pointer">
-//                                    <i class="fas fa-edit"></i> 
-//                                </a>
-//                                <a onClick=Delete("/Admin/Category/Delete/${data}") class="btn btn-danger text-white" style="cursor:pointer">
-//                                    <i class="fas fa-trash-alt"></i> 
-//                                </a>
-//                            </div>
-//                           `;
-//                }, "width": "40%"
-//            }
+            { "data": "name", "width": "15%" },
+            { "data": "email", "width": "15%" },
+            { "data": "phoneNumber", "width": "15%" },
+            { "data": "company.name", "width": "15%" },
+            { "data": "role", "width": "15%" },
+            {
+                "data": {id:"id", lockoutEnd:"lockoutEnd"},
+                "render": function (data) {
+                    var today = new Date().getTime();
+                    var lockout = new Date(data.lockoutEnd).getTime();
+                    if (lockout > today) {
+                        // user is currently locked
+
+                        return `
+                            <div class="text-center">
+                                
+                                <a onClick=LockUnlock("${data.id}") class="btn btn-danger text-white" style="cursor:pointer">
+                                    <i class="fas fa-lock-open mr-1"></i> Unlock
+                                </a>
+                            </div>
+                           `;
+                    } else {
+                        // user is currently unlocked
+
+                        return `
+                            <div class="text-center">
+                                
+                                <a onClick=LockUnlock("${data.id}") class="btn btn-success text-white" style="cursor:pointer">
+                                    <i class="fas fa-lock"></i> Lock
+                                </a>
+                            </div>
+                           `;
+                    }
+                    
+                }, "width": "25%"
+            }
         ]
     });
 }
 
-function Delete(url) {
-    swal({
-        title: "Are you sure you want to delete?",
-        text: "You will not be able to restore the data!",
-        icon: "warning",
-        buttons: true,
-        dangerMode: true
-    }).then((willDelete) => {
-        if (willDelete) {
-            $.ajax({
-                type: "Delete",
-                url: url,
-                success: function(data) {
-                    if (data.success) {
-                        toastr.success(data.message);
-                        dataTable.ajax.reload();
-                    } else {
-                        toastr.error(data.message);
-                    }
-                }
-            });
-        }
-    });
-}
