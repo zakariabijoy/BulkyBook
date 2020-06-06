fc6684a9a23f0f56548c7cbc6bc552f443903cb6
zakariabijoy <zakaria.bijoy@live.com>
2020-06-07 03:59:53 +0600

    Section: Emails ; Implemeted smtp mail service for confirm email after user registration

--- BulkyBook.Uility/EmailSender.cs
+++ BulkyBook.Uility/EmailSender.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity.UI.Services;
@@ -10,7 +12,19 @@ namespace BulkyBook.Uility
     {
         public Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
-            throw new NotImplementedException();
+            var client = new SmtpClient("smtp.mailtrap.io", 2525)
+            {
+                Credentials = new NetworkCredential("1e8c185c2383b1", "8ee6408a1d3161"),
+                EnableSsl = true
+            };
+            var mail = new MailMessage();
+            mail.IsBodyHtml = true;
+            mail.From = new MailAddress("BulkyBook@gmail.com", "BulkyBook");
+            mail.To.Add(email);
+            mail.Subject = subject;
+            mail.Body = htmlMessage;
+
+            return client.SendMailAsync(mail);
         }
     }
 }
