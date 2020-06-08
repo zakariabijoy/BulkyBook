c0b76e8d61055001583efd40126cffd1c9ff22bd
zakariabijoy <zakaria.bijoy@live.com>
2020-06-09 03:03:37 +0600

    Section: ShoppingCartManagement; -- Cart Index Action Implemented for showing list of shopping cart with other details

--- BulkyBook.Uility/SD.cs
+++ BulkyBook.Uility/SD.cs
@@ -19,5 +19,61 @@ namespace BulkyBook.Uility
 
 
         public const string Session_ShoppingCart = "ShoppingCart Session";
+
+        public static double GetPriceBasedOnQuatity( int quantity,  double price,  double price50,  double price100)
+        {
+            if (quantity < 50)
+            {
+                return price;
+            }
+            else
+            {
+                if (quantity < 100)
+                {
+                    return price50;
+                }
+                else
+                {
+                    return price100;
+                }
+                    
+               
+            }
+                
+           
+        }
+
+        public static string ConvertToRawHtml(string source)
+        {
+            char [] array = new char[source.Length];
+            int arrayIndex = 0;
+            bool inside = false;
+
+            for (int i = 0; i < source.Length; i++)
+            {
+                char let = source[i];
+
+                if (let == '<')
+                {
+                    inside = true;
+                    continue;
+                }
+
+                if (let == '>')
+                {
+                    inside = false;
+                    continue;
+                }
+
+                if (!inside)
+                {
+                    array[arrayIndex] = let;
+                    arrayIndex++;
+                }
+            }
+
+            return new string(array,0,arrayIndex);
+
+        }
     }
 }
---
+++ BulkyBook/Areas/Customer/Controllers/CartController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using BulkyBook.Models.ViewModels;
+using BulkyBook.Uility;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBook.Areas.Customer.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IEmailSender _emailSender;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public ShoppingCartVM ShoppingCartVm { get; set; }
+
+        public CartController(IUnitOfWork unitOfWork, IEmailSender emailSender, UserManager<IdentityUser> userManager)
+        {
+            _unitOfWork = unitOfWork;
+            _emailSender = emailSender;
+            _userManager = userManager;
+        }
+        public IActionResult Index()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            ShoppingCartVm = new ShoppingCartVM()
+            {
+                ShoppingCarts = _unitOfWork.ShoppingCart
+                                    .GetAll(s => s.ApplicationUserId == claim.Value, 
+                                        includeProperties: "Product"),
+
+                OrderHeader = new OrderHeader()
+            };
+
+            ShoppingCartVm.OrderHeader.OrderTotal = 0;
+            ShoppingCartVm.OrderHeader.ApplicationUser = _unitOfWork.ApplicationUser
+                                                             .GetFirstOrDefault(a => a.Id == claim.Value, 
+                                                                 includeProperties: "Company");
+
+            foreach (var shoppingCart in ShoppingCartVm.ShoppingCarts)
+            {
+                shoppingCart.Price = SD.GetPriceBasedOnQuatity(shoppingCart.Count, shoppingCart.Product.Price,
+                                                    shoppingCart.Product.Price50, shoppingCart.Product.Price100);
+
+                ShoppingCartVm.OrderHeader.OrderTotal += (shoppingCart.Price * shoppingCart.Count);
+
+                shoppingCart.Product.Description = SD.ConvertToRawHtml(shoppingCart.Product.Description);
+
+                if (shoppingCart.Product.Description.Length > 100)
+                {
+                    shoppingCart.Product.Description = shoppingCart.Product.Description.Substring(0, 99) + "...";
+                }
+            }
+                    
+            return View(ShoppingCartVm);
+        }
+    }
+}
