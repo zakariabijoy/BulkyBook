3c744ff6ce2a8f8d91053bd9e7c33ace1d77da92
zakariabijoy <zakaria.bijoy@live.com>
2020-07-07 03:15:36 +0600

    Section : Summary and Payments - added validation for summary

--- BulkyBook.Models/OrderHeader.cs
+++ BulkyBook.Models/OrderHeader.cs
@@ -27,11 +27,17 @@ namespace BulkyBook.Models
         public DateTime PaymentDueDate { get; set; }
         public string TransactionId{ get; set; }
 
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string PhoneNumber { get; set; }
+        [Required]
         public string StreetAddress { get; set; }
+        [Required]
         public string City { get; set; }
+        [Required]
         public string State { get; set; }
+        [Required]
         public string PostalCode { get; set; }
     }
 }
