3a0078d7060980078a177d633c7b4cc09fca89a3
zakariabijoy <zakaria.bijoy@live.com>
2020-07-15 18:13:39 +0600

    setting up twilio for sending api

---
+++ BulkyBook.Uility/TwilioSettings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.UI.Services;
+
+namespace BulkyBook.Uility
+{
+    public class TwilioSettings
+    {
+        public string PhoneNumber { get; set; }
+        public string AccountSid { get; set; }
+        public string AuthToken { get; set; }
+       
+    }
+}
