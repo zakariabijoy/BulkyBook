1c0efcb1d33612427bd7c4fe2fb2382d013ac327
zakariabijoy <zakaria@vonome.com>
2020-07-16 01:59:21 +0600

    Model Added For Custome Tag helper for paging

---
+++ BulkyBook.Models/PagingInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BulkyBook.Models
+{
+    public class PagingInfo
+    {
+        public int TotalItem { get; set; }
+        public int ItemPerPage { get; set; }
+        public int CurrentPage { get; set; }
+        public string UrlParam { get; set; }
+
+        public int TotalPage => (int) Math.Ceiling((decimal) TotalItem / ItemPerPage);
+    }
+}
